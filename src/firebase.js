f9ad6c6ef20bf29ec4ac2c66e812e8ff09f9d09c
Crystallizee <benidictus.tf@yahoo.com>
2021-11-14 20:48:57 +0700

    guided

---
+++ src/firebase.js
@@ -0,0 +1,7 @@
+import firebase from "firebase/app";
+import "firebase/database";
+
+export const db = firebase.initializeApp({ databaseURL: "https://guidedvuefire0235-default-rtdb.firebaseio.com" }).database();
+
+export const drinkRef = db.ref("drinks");
+    
